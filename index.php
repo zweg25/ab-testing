32b78bf31ef50587fe926c5fff3fbc738224053e
Zak Wegweiser <zweg25@aol.com>
2017-10-17 23:43:26 -0400

    Updated index.php
    Added styles

--- index.php
+++ index.php
@@ -1,13 +1,48 @@
-<?php
-echo "<b>Here are all the files you can access:</b>";
-$path = ".";
-$dh = opendir($path);
-$i=1;
-while (($file = readdir($dh)) !== false) {
-    if($file != "." && $file != ".." && $file != "index.php" && $file != ".htaccess" && $file != "error_log" && $file != "cgi-bin") {
-        echo "<a href='$path/$file'>$file</a><br /><br />";
-        $i++;
-    }
-}
-closedir($dh);
-?> 
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Zak's AB Testing</title>
+	<style type="text/css">
+		.vertical-menu {
+		    width: 300px; /* Set a width if you like */
+		}
+
+		.vertical-menu a {
+		    background-color: #eee; /* Grey background color */
+		    color: black; /* Black text color */
+		    display: block; /* Make the links appear below each other */
+		    padding: 12px; /* Add some padding */
+		    text-decoration: none; /* Remove underline from links */
+		}
+
+		.vertical-menu a:hover {
+		    background-color: #ccc; /* Dark grey background on mouse-over */
+		}
+
+		.vertical-menu a.active {
+		    background-color: #FF4500; /* Add a orange color to the current link */
+		    color: black;
+		}
+	</style>
+</head>
+<body>
+	<div class="vertical-menu">
+		<a href="#" class="active">Home</a>
+		<?php
+		echo "<h2>Check out:</h2>";
+		$path = ".Code/";
+		$dh = opendir($path);
+		$i=1;
+		while (($file = readdir($dh)) !== false) {
+		    if($file != "." && $file != ".." && $file != "index.php" && $file != ".htaccess" && $file != "error_log" && $file != "cgi-bin") {
+		        echo "<a href='$path/$file'>$file</a><br /><br />";
+		        $i++;
+		    }
+		}
+		closedir($dh);
+		?>
+	</div>
+</body>
+</html>
+
